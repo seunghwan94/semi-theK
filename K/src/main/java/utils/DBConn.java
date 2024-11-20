dfb1be3a08e3fa63b88377f35ae438a096e5d246
Sangwon1233 <tkddnjs3351@naver.com>
2024-11-20 18:31:25 +0900

    jsp db connent

---
+++ K/src/main/java/utils/DBConn.java
@@ -0,0 +1,38 @@
+package utils;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConn {
+	public static Connection getConnection() throws ClassNotFoundException,SQLException{
+
+			Properties props= new Properties();
+			
+			InputStream  is = DBConn.class.getClassLoader().getResourceAsStream("db.properties");
+			try {
+				props.load(is);
+			}
+			catch (IOException e) {
+				e.printStackTrace();// TODO: handle exception
+			}
+			String driver=props.getProperty("driver");
+			String host=props.getProperty("url");
+			String username=props.getProperty("username");
+			String password=props.getProperty("password");
+			Class.forName(driver);
+			return DriverManager.getConnection(host,username,password);
+			
+
+	}	
+	public static void main(String[] args) throws Exception{
+		System.out.println(getConnection());
+	}
+		
+
+}
+
+
+
