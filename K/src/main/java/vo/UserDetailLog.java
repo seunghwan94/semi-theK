9746ddca699bc0c4bce0f846101e299723e1dfcb
Sangwon1233 <tkddnjs3351@naver.com>
2024-11-21 14:31:46 +0900

    유저 필드

--- K/src/main/java/vo/UserDetailLog.java
+++ K/src/main/java/vo/UserDetailLog.java
@@ -2,6 +2,15 @@ package vo;
 
 import java.sql.Date;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserDetailLog {
 	public int loguserdetailno;
 	public String userid;
