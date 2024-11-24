41746d62c73dd83d39584d6eb0a74d02c99d7517
DahnDell <khanman1208@gmail.com>
2024-11-24 14:33:40 +0900

    환경설정 및 파일 오류 수정

--- src/main/java/mapper/PostMapper.java
+++ src/main/java/mapper/PostMapper.java
@@ -1,6 +1,6 @@
 package mapper;
 
-import java.util.List;
+//import java.util.List;
 
 import org.apache.ibatis.annotations.Select;
 
--- src/main/java/vo/Post.java
+++ src/main/java/vo/Post.java
@@ -1,15 +1,16 @@
 package vo;
 
 import java.sql.Date;
-import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class Post {
 	private final int pno; // 게시글 번호
 	private final String title; // 게시글 제목
@@ -19,6 +20,21 @@ public class Post {
 	private final Date createDate; // 게시글 등록일
 	private final Date updateDate; // 게시글 업데이트 날짜
 	private final int cno; // 게시글 카테고리 번호(외래키)
+	
+	public Post(int pno, String title, String userId, String content, int viewCnt, Date createDate, Date updateDate,
+			int cno) {
+		super();
+		this.pno = pno;
+		this.title = title;
+		this.userId = userId;
+		this.content = content;
+		this.viewCnt = viewCnt;
+		this.createDate = createDate;
+		this.updateDate = updateDate;
+		this.cno = cno;
+	}
+	
+//	
 	
 //	private final boolean attachFlag;
 	
