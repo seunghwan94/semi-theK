32896b476b8d4f787c7a196da4188cffc02001a1
DahnDell <khanman1208@gmail.com>
2024-11-26 20:28:56 +0900

    Quil 사용 구현중

--- src/main/java/servlet/post/Write.java
+++ src/main/java/servlet/post/Write.java
@@ -1,5 +1,34 @@
 package servlet.post;
 
-public class Write {
+import java.io.IOException;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/post/write")
+public class Write extends HttpServlet{
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		req.getParameter("pno");
+		req.getRequestDispatcher("/WEB-INF/k/post/write.jsp").forward(req, resp);
+//		req.setAttribute("criteria", criteria);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		
+		String title = req.getParameter("title"); 
+		String content = req.getParameter("content"); 
+		String writer  = req.getParameter("writer");
+		
+		
+		
+	}
+	
+	
+	
 }
