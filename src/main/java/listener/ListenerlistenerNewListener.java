f9db93f4fc4c1742e2d5dc074f98d2ccd2932761
DahnDell <khanman1208@gmail.com>
2024-11-28 21:10:55 +0900

    Listner?

---
+++ src/main/java/listener/ListenerlistenerNewListener.java
@@ -0,0 +1,19 @@
+//package listener;
+//
+//import javax.servlet.annotation.WebListener;
+//import javax.servlet.http.HttpSessionEvent;
+//import javax.servlet.http.HttpSessionListener;
+//
+//import service.UserService;
+//import service.UserServiceImpl;
+//
+//@WebListener
+//public class ListenerlistenerNewListener implements HttpSessionListener{
+//    private UserService service = UserServiceImpl.getInstance();
+//
+//    @Override
+//    public void sessionCreated(HttpSessionEvent se) {
+//    }
+//}
+
+
